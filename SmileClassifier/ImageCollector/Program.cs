4cc56b5c646ffcbd65e0f4a0a5ec7158f71cc2d0
garicchi <xgaricchi@gmail.com>
2016-09-19 06:16:46 +0900

    complete smile classifer

---
+++ SmileClassifier/ImageCollector/Program.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCollector
+{
+    class Program
+    {
+        static string bingApiKey = "6b92b8aafcf4408fbab7ba3bab0550c8";
+        static string faceApiKey = "275f7ae3c0ca42fda3eca8bee0956fad";
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            var learnDataPath = "data.csv";
+            Task.Run(async () =>
+            {
+
+                using (var writer = new StreamWriter(learnDataPath, false, Encoding.UTF8))
+                {
+                    for(int i = 1; i <= 12; i++)
+                    {
+                        writer.Write(string.Format("param{0},",i));
+                    }
+                    writer.WriteLine("label");
+                    var smileImages = await getSearchImageAsync("笑顔");
+                    foreach (var url in smileImages)
+                    {
+                        try
+                        {
+                            var client = new FaceServiceClient(faceApiKey);
+                            var faces = await client.DetectAsync(url, true, true);
+                            await Task.Delay(4000);
+                            foreach (var face in faces)
+                            {
+                                var features = getFaceFeature(face);
+                                writer.WriteLine(string.Join(",", features) + ",smile");
+                                Console.WriteLine("detect feature {0}", url);
+                            }
+                        }
+                        catch (FaceAPIException e)
+                        {
+                            Console.WriteLine("face expception");
+                        }
+                    }
+
+                    var angryImages = await getSearchImageAsync("怒り 写真");
+                    foreach (var url in angryImages)
+                    {
+                        try
+                        {
+                            var client = new FaceServiceClient(faceApiKey);
+                            var faces = await client.DetectAsync(url, true, true);
+                            await Task.Delay(4000);
+                            foreach (var face in faces)
+                            {
+                                var features = getFaceFeature(face);
+                                writer.WriteLine(string.Join(",", features) + ",angry");
+                                Console.WriteLine("detect feature {0}", url);
+                            }
+                        }
+                        catch (FaceAPIException e)
+                        {
+                            Console.WriteLine("face expception");
+                        }
+                    }
+                }
+
+            }).Wait();
+            Console.WriteLine();
+            Console.WriteLine("elapsed time {0}",(DateTime.Now - start).ToString());
+            Console.ReadKey();
+        }
+
+        private static async Task<List<string>> getSearchImageAsync(string searchWord)
+        {
+            var images = new List<string>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", bingApiKey);
+
+                int count = 150;
+                for (int offset = 0; offset <= 300; offset += count)
+                {
+                    var uri = "https://api.cognitive.microsoft.com/bing/v5.0/images/search"
+                        + "?q=" + searchWord
+                        + "&count=" + count
+                        + "&offset=" + offset
+                        + "&mkt=ja-JP";
+
+                    var response = await client.GetAsync(uri);
+                    var json = await response.Content.ReadAsStringAsync();
+                    var jObj = JObject.Parse(json);
+                    JToken value = jObj.SelectToken("value");
+                    images.AddRange(value.Select(q => q["contentUrl"].ToString()).ToList());
+                    await Task.Delay(400);
+                }
+            }
+                
+            return images;
+        }
+
+        private static List<double> getFaceFeature(Face face)
+        {
+            var result = new List<double>();
+            var marks = face.FaceLandmarks;
+
+            //Topから目までの長さの顔の長さとの比
+            result.Add((marks.EyeRightTop.Y - face.FaceRectangle.Top) / face.FaceRectangle.Height);
+            //目から鼻までの長さの顔の長さとの比
+            result.Add((marks.NoseTip.Y - marks.EyeRightTop.Y) / face.FaceRectangle.Height);
+            //鼻から顎までの長さの顔の長さとの比
+            result.Add((face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y)
+                / face.FaceRectangle.Height);
+            //下唇の位置の鼻から顎までの長さとの比(上)
+            result.Add((marks.UnderLipBottom.Y - marks.NoseTip.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+            //下唇の位置の鼻から顎までの長さとの比(下)
+            result.Add((face.FaceRectangle.Top + face.FaceRectangle.Height - marks.UnderLipBottom.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+            //唇の位置の鼻から顎までの長さとの比(上)
+            result.Add((marks.MouthRight.Y - marks.NoseTip.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+            //唇の位置の鼻から顎までの長さとの比(下)
+            result.Add((face.FaceRectangle.Top + face.FaceRectangle.Height - marks.MouthRight.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+
+            //左耳から左目までの顔の横幅との比
+            result.Add((marks.EyeLeftOuter.X - face.FaceRectangle.Left) / face.FaceRectangle.Width);
+            //左目の幅の顔の横幅との比
+            result.Add((marks.EyeLeftInner.X - marks.EyeLeftOuter.X) / face.FaceRectangle.Width);
+            //目の間の顔の横幅との比
+            result.Add((marks.EyeRightInner.X - marks.EyeLeftInner.X) / face.FaceRectangle.Width);
+            //右目の幅の顔の横幅との比
+            result.Add((marks.EyeRightOuter.X - marks.EyeRightInner.X) / face.FaceRectangle.Width);
+            //右目から右耳までの顔の横幅との比
+            result.Add((face.FaceRectangle.Left + face.FaceRectangle.Width - marks.EyeRightOuter.X)
+                / face.FaceRectangle.Width);
+
+            return result;
+
+        }
+
+
+    }
+}
--- SmileClassifier/SmileClassifier/MainPage.xaml.cs
+++ SmileClassifier/SmileClassifier/MainPage.xaml.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,6 +38,7 @@ namespace SmileClassifier
         //Webカメラのキャプチャをするクラス
         MediaCapture _mediaCapture;
         MediaCaptureInitializationSettings setting;
+        string apiKey = "275f7ae3c0ca42fda3eca8bee0956fad";
 
         public MainPage()
         {
@@ -64,7 +75,7 @@ namespace SmileClassifier
                 {
                     //デバイス一覧からビデオキャプチャーができるデバイスを取得する
                     DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
-                    DeviceInformation cameraId = devices.ElementAt(0);
+                    DeviceInformation cameraId = devices.ElementAt(1);
                     //設定に取得したカメラデバイスのIDを登録する
                     setting = new MediaCaptureInitializationSettings();
                     setting.VideoDeviceId = cameraId.Id;
@@ -80,6 +91,122 @@ namespace SmileClassifier
                     Debug.WriteLine(ex.Message);
                 }
             });
+        }
+
+        private static List<double> getFaceFeature(Face face)
+        {
+            var result = new List<double>();
+            var marks = face.FaceLandmarks;
+
+            //Topから目までの長さの顔の長さとの比
+            result.Add((marks.EyeRightTop.Y - face.FaceRectangle.Top) / face.FaceRectangle.Height);
+            //目から鼻までの長さの顔の長さとの比
+            result.Add((marks.NoseTip.Y - marks.EyeRightTop.Y) / face.FaceRectangle.Height);
+            //鼻から顎までの長さの顔の長さとの比
+            result.Add((face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y)
+                / face.FaceRectangle.Height);
+            //下唇の位置の鼻から顎までの長さとの比(上)
+            result.Add((marks.UnderLipBottom.Y - marks.NoseTip.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+            //下唇の位置の鼻から顎までの長さとの比(下)
+            result.Add((face.FaceRectangle.Top + face.FaceRectangle.Height - marks.UnderLipBottom.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+            //唇の位置の鼻から顎までの長さとの比(上)
+            result.Add((marks.MouthRight.Y - marks.NoseTip.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+            //唇の位置の鼻から顎までの長さとの比(下)
+            result.Add((face.FaceRectangle.Top + face.FaceRectangle.Height - marks.MouthRight.Y)
+                / (face.FaceRectangle.Top + face.FaceRectangle.Height - marks.NoseTip.Y));
+
+            //左耳から左目までの顔の横幅との比
+            result.Add((marks.EyeLeftOuter.X - face.FaceRectangle.Left) / face.FaceRectangle.Width);
+            //左目の幅の顔の横幅との比
+            result.Add((marks.EyeLeftInner.X - marks.EyeLeftOuter.X) / face.FaceRectangle.Width);
+            //目の間の顔の横幅との比
+            result.Add((marks.EyeRightInner.X - marks.EyeLeftInner.X) / face.FaceRectangle.Width);
+            //右目の幅の顔の横幅との比
+            result.Add((marks.EyeRightOuter.X - marks.EyeRightInner.X) / face.FaceRectangle.Width);
+            //右目から右耳までの顔の横幅との比
+            result.Add((face.FaceRectangle.Left + face.FaceRectangle.Width - marks.EyeRightOuter.X) 
+                / face.FaceRectangle.Width);
+
+            return result;
+
+        }
+
+
+        private async void captureElement_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (_mediaCapture.VideoDeviceController.FocusControl.Supported)
+            {
+                await _mediaCapture.VideoDeviceController.FocusControl.FocusAsync();
+            }
+            
+            var list = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview).Properties.ToList();
+            var stream = new InMemoryRandomAccessStream();
+            var prop = ImageEncodingProperties.CreatePng();
+            prop.Width = (uint)captureElement.ActualWidth;
+            prop.Height = (uint)captureElement.ActualHeight;
+            await _mediaCapture.CapturePhotoToStreamAsync(prop, stream);
+            stream.Seek(0);
+
+            var faceClient = new FaceServiceClient(apiKey);
+            var faces = await faceClient.DetectAsync(stream.AsStream(), true, true);
+            if (faces.Count() > 0)
+            {
+                var face = faces.First();
+                var paramList = getFaceFeature(face);
+                using (var client = new HttpClient())
+                {
+                    StringBuilder sb = new StringBuilder();
+                    StringWriter sw = new StringWriter(sb);
+
+                    using (var w = new JsonTextWriter(sw))
+                    {
+                        w.WriteStartObject();
+                        w.WritePropertyName("Inputs");
+                        w.WriteStartObject();
+                        w.WritePropertyName("input1");
+                        w.WriteStartArray();
+                        w.WriteStartObject();
+                        for (int i = 0; i < paramList.Count; i++)
+                        {
+                            w.WritePropertyName("param" + (i+1));
+                            w.WriteValue(paramList[i]);
+                        }
+                        w.WritePropertyName("label");
+                        w.WriteValue("null");
+                        w.WriteEndObject();
+
+                        w.WriteEndArray();
+                        w.WriteEndObject();
+                        w.WritePropertyName("GlobalParameters");
+                        w.WriteStartObject();
+                        w.WriteEndObject();
+                        w.WriteEndObject();
+                    }
+                    var json = sb.ToString();
+
+                    var apiKey = "vdONRbDzchAzdzlmr+MGez+xw67O0uPIrMng1FrKMOiZlr5sWSHus7Ja+NQiDubDc7BrxattCi2fnDGPyCxvYA=="; // Replace this with the API key for the web service
+                    var requestUrl = "https://asiasoutheast.services.azureml.net/subscriptions/25116a6966a94419a84024e51e3fc3ee/services/23115b24669d401b936a44901a754c25/execute?api-version=2.0&format=swagger";
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                    var content = new StringContent(json,Encoding.UTF8,"applicaton/json");
+                    var response = await client.PostAsync(requestUrl, content);
+                    var jsonResult = await response.Content.ReadAsStringAsync();
+                    var jObj = JObject.Parse(jsonResult);
+                    var label = jObj.SelectToken("Results.output1[0]['Scored Labels']").Value<string>();
+                        //.SelectToken("Scored Labels");
+                    Debug.WriteLine(label);
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog("顔検出に失敗しました");
+                await dialog.ShowAsync();
+            }
+
+            
+
         }
     }
 }
